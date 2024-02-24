fe5c478588f2656fed072a5fc00baac4827288b3
pianofreq <39473249+pianofreq@users.noreply.github.com>
2024-02-23 23:24:24 -0500

    Create placeholder app.js
    a helloworld app to test kinsta buildpacks

---
+++ app.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+// Create an HTTP server
+const server = http.createServer((req, res) => {
+  // Set the response HTTP header with HTTP status and Content type
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+
+  // Send the response body "Hello World"
+  res.end('Hello World\n');
+});
+
+// Listen on port 3000, IP defaults to 127.0.0.1 (localhost)
+server.listen(3000, () => {
+  console.log('Server running at http://127.0.0.1:3000/');
+});
